037bff7cb8b1d3501b244409a286fc761fb00a74
AromaDsouza <aromadsouza8@gmail.com>
2022-12-05 23:02:43 +0000

    third commit

--- statistics_and_trends.py
+++ statistics_and_trends.py
@@ -4,51 +4,72 @@ Created on Sun Dec  4 00:09:06 2022
 
 @author: Aroma
 """
-
-import pandas as pd  #To open the files
-import matplotlib.pyplot as plt  #Used for vizualizations
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def urban_population(reading_the_file):
         dataframe1 = pd.read_csv(reading_the_file,skiprows=4) #first read the data,convert to dataframe
-        #dataframe1 = dataframe1.iloc[11:20,[0,40,50,60]]
-        dataframe1 = dataframe1.filter(items=[10,22,35,40,77,81,109,119], axis=0)
-        dataframe1 = dataframe1.fillna(0)
+        #dataframe1 = dataframe1.iloc[[11:20],[0,40,50,60]]
+        dataframe1 = dataframe1.filter(items=[35,40,77,81,109,113,119,126], axis=0)
+        dataframe1 = dataframe1.drop(['Country Code','Indicator Name','Indicator Code'],axis=1)
+        #dataframe1 = dataframe1.filter(items=['Country Name','2000','2001','2002','2003'], axis=1)
+        dataframe1 = dataframe1.dropna(axis=1)
         dataframe2 = dataframe1.T
-        print(dataframe1)
-        print(dataframe2)
+        #print(dataframe1,dataframe2)
+        return dataframe1, dataframe2
+    
+a,b = urban_population("Urban population.csv")
+print(a)
+print(b)       
         
-        plt.figure(figsize=(20,10),dpi=144)
-        plt.plot(dataframe1['Country Name'],dataframe1["1986"],label="1996")
-        plt.plot(dataframe1['Country Name'],dataframe1["1996"],label="2006")
-        plt.plot(dataframe1['Country Name'],dataframe1["2006"],label="2006")
-        plt.plot(dataframe1['Country Name'],dataframe1["2016"],label="2016")
-        plt.xlabel("Countries",fontsize=25)  #To label the x-axis
-        #plt.ylabel("Urban Population (%)")  #To label the y-axis
-        plt.xticks(rotation=45)
-        plt.title("Urban Population (%)",fontsize=25) #To mention the title for stacked bar graph
-        plt.legend(loc='upper right',fontsize=20)  #To show the label names in form of a box
-        plt.savefig('Line graph.png') #Saving the image of the line graph
-        plt.show()
-      
-print(urban_population("Urban population.csv"))
+plt.figure(figsize=(20,10),dpi=144)
+plt.plot(a['Country Name'],a["2000"],label="2000")
+plt.plot(a['Country Name'],a["2001"],label="2001")
+plt.plot(a['Country Name'],a["2002"],label="2002")
+plt.plot(a['Country Name'],a["2003"],label="2003")
+plt.xlabel("Countries",fontsize=25)  #To label the x-axis
+plt.xticks(rotation=45)
+plt.title("Urban Population (%)",fontsize=25) #To mention the title for bar graph
+plt.legend(loc='upper right',fontsize=20)  #To show the label names in form of a box
+plt.savefig('Line graph.png') #Saving the image of the line graph
+plt.show()
 
 def green_house_gases(read_the_file):
         dataframe1 = pd.read_csv(read_the_file,skiprows=4) #first read the data,convert to dataframe
-        dataframe1 = dataframe1.iloc[111:120, [0,20,30,40,50,60]]       
+        dataframe1 = dataframe1.iloc[[35,40,77,81,109,113,119,126],[0,20,30,40,50,60]]
         #dataframe1 = dataframe1.filter(items=[10,22,35,40,77,81,109,119], axis=0)
-        dataframe1 = dataframe1.fillna(0)
+        dataframe1 = dataframe1.dropna(axis=1)
         dataframe2 = dataframe1.T
-        print(dataframe1)
-        print(dataframe2)
-        
-        dataframe1.plot(kind="bar", x = 'Country Name', rot=0, figsize= (20,20))
-        plt.xlabel("Countries",fontsize=25)  #To label the x-axis
-        #plt.ylabel("Urban Population (%)")  #To label the y-axis
-        plt.title("Urban Population (%)",fontsize=25) #To mention the title for stacked bar graph
-        plt.legend(loc='upper right',fontsize=20)  #To show the label names in form of a box
-        plt.savefig('Side by side bar graph.png') #Saving the image of the line graph
-        plt.show()  #To display the image of the line graph
-        #plt.xticks(rotation=5)
-        return dataframe1, dataframe2   
-     
-print(green_house_gases("Greenhouse gases.csv"))
+        #print(dataframe1,dataframe2)
+        return dataframe1, dataframe2 
+
+c,d = green_house_gases("Greenhouse gases.csv")
+print(c)
+print(d)
+
+c.plot(kind="bar", x = 'Country Name', rot=0, figsize= (10,10))
+plt.xlabel("Countries",fontsize=25)  #To label the x-axis
+plt.title("Urban Population (%)",fontsize=25) #To mention the title for stacked bar graph
+plt.legend(loc='upper right',fontsize=20)  #To show the label names in form of a box
+plt.savefig('Side by side bar graph.png') #Saving the image of the line graph
+plt.show()  #To display the image of the line graph
+
+def forest_area(read_the_file):
+        dataframe1 = pd.read_csv(read_the_file,skiprows=4) #first read the data,convert to dataframe
+        dataframe1 = dataframe1.iloc[[35,40,77,81,109,113,119,126],[0,20,30,40,50,60]]
+        #dataframe1 = dataframe1.filter(items=[10,22,35,40,77,81,109,119], axis=0)
+        dataframe1 = dataframe1.dropna(axis=1)
+        dataframe2 = dataframe1.T
+        print(dataframe1,dataframe2)
+        return dataframe1, dataframe2 
+
+e,f = green_house_gases("Forest Area in sq km.csv")
+print(e)
+print(f)
+
+e.plot(kind="bar", x = 'Country Name', rot=0, figsize= (10,10))
+plt.xlabel("Countries",fontsize=25)  #To label the x-axis
+plt.title("Forest Area (Sq Km)",fontsize=25) #To mention the title for stacked bar graph
+plt.legend(loc='upper right',fontsize=20)  #To show the label names in form of a box
+plt.savefig('Side by side bar graph1.png') #Saving the image of the line graph
+plt.show()  #To display the image of the line graph
