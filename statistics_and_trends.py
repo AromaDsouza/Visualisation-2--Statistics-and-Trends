2e83f291cf4936218ee0541868f83fdf84631e56
AromaDsouza <aromadsouza8@gmail.com>
2022-12-08 00:14:43 +0000

    fifth commit

--- statistics_and_trends.py
+++ statistics_and_trends.py
@@ -53,22 +53,27 @@ plt.savefig('Side by side bar graph1.png') #Saving the image of the line graph
 plt.show()  #To display the image of the line graph
 
 def forest_area(read_the_file):
-    dataframe1 = pd.read_csv(read_the_file,skiprows=4) #first read the data,convert to dataframe
-    dataframe1 = dataframe1.iloc[[35,40,77,81,109,113,119,126],[0,20,30,40,50,60]]
-    #dataframe1 = dataframe1.filter(items=[10,22,35,40,77,81,109,119], axis=0)
-    dataframe1 = dataframe1.dropna(axis=1)
-    dataframe2 = dataframe1.T
-    print(dataframe1,dataframe2)
-    return dataframe1, dataframe2
-e,f = green_house_gases("Forest Area in sq km.csv") 
+        dataframe1 = pd.read_csv(read_the_file,skiprows=4) #first read the data,convert to dataframe
+        dataframe1 = dataframe1.iloc[[35,40,77,81,109,113,119,126],[0,20,30,40,50,60]]
+        #dataframe1 = dataframe1.filter(items=[10,22,35,40,77,81,109,119], axis=0)
+        dataframe1 = dataframe1.dropna(axis=1)
+        dataframe2 = dataframe1.set_index('Country Name').T
+        #rint(dataframe1,dataframe2)
+        return dataframe1, dataframe2 
+
+e,f = forest_area("Forest Area in sq km.csv")
 print(e)
 print(f)
-e.plot(kind="bar", x = 'Country Name', rot=0, figsize= (10,10))
+e["Avg"] = e.iloc[:, 1:5].mean(axis=1)
+print(e)
+print(f)
+e.plot(kind="pie",rot= 0, figsize= (20,10),y = 'Avg',labels = e['Country Name'])
 plt.xlabel("Countries",fontsize=25)  #To label the x-axis
-plt.title("Forest Area (Sq Km)",fontsize=25) #To mention the title for stacked bar graph
-plt.legend(loc='upper right',fontsize=20)  #To show the label names in form of a box
-plt.savefig('Side by side bar graph2.png') #Saving the image of the line graph
-plt.show()  #To display the image of the line graph
+#plt.xticks(rotation=45)
+plt.title("Forest Area in sq km", fontsize=25)  #To display the image of the pie graph
+#plt.legend(loc='upper right',fontsize=10)#To show the label names in form of a box
+plt.savefig('Pie chart.png') #Saving the image of the line graph
+plt.show()
 
 def renewable_energy(read_the_file):
     dataframe1 = pd.read_csv(read_the_file,skiprows=4) #first read the data,convert to dataframe
@@ -86,6 +91,7 @@ h.plot(kind="line", rot= 0, figsize= (20,10))
 plt.xlabel("Countries",fontsize=25)  #To label the x-axis
 #plt.xticks(rotation=45)
 plt.title("Renewable Energy (%)",fontsize=25) #To mention the title for bar graph
+
 plt.legend(loc='best',fontsize=20)  #To show the label names in form of a box
 plt.savefig('Line graph2.png') #Saving the image of the line graph
 plt.show()
